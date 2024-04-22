74de0e6f3342d9e683ae183e5f8cbbe7dc784cea
kelvinehiz <kelvinehiz@gmail.com>
2024-04-22 22:22:41 +0000

    removed whitespace

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,4 +13,6 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, t.get("completed"), t.get("title")]) for t in todos]
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
