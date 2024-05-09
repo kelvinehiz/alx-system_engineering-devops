c80533ad65f3cda2a4dd8b57ef16383851a8f99c
kelvinehiz <kelvinehiz@gmail.com>
2024-05-09 15:46:48 +0000

    implementing corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Contains the number_of_subscribers function"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if not subreddit or not isinstance(subreddit, str):
-        return 0
 
-    try:
-        response = requests.get(
-            f'http://www.reddit.com/r/{subreddit}/about.json',
-            headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-        )
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
