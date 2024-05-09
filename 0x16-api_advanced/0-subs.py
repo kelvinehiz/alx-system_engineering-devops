048585b242809acb66d08008d6a6d45ced04dc19
kelvinehiz <kelvinehiz@gmail.com>
2024-05-09 15:35:23 +0000

    implementing corrections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Contains the number_of_subscribers function"""
 import requests
-
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    try:
+        response = requests.get(
+            f'http://www.reddit.com/r/{subreddit}/about.json',
+            headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+        )
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
+        return subscribers
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
